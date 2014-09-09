fe479e0aad955cba081f2985193fe5ec5100ef0f
sindsri <sindhujas.psgtech@gmail.com>
2014-09-09 01:01:39 -0700

    code path week 1 iOS app
    code path week 1 iOs tip calculator

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Sindhuja Sridharan on 9/8/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
