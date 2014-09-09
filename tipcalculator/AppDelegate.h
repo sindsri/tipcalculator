b6e00decabb3fc8de13582477554de7a32f43618
Sindhuja Sridharan <ssridharan@thinkfair-lm.corp.yahoo.com>
2014-09-08 23:44:12 -0700

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Sindhuja Sridharan on 9/8/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
